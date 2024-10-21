a090bfbd6b7c4a254f1f697bd99f5dadc65f05cf
Phil Wang <lucidrains@gmail.com>
2024-10-21 07:28:47 -0700

    first the encoder and decoder

--- spline_based_transformer/spline_based_transformer.py
+++ spline_based_transformer/spline_based_transformer.py
@@ -2,9 +2,12 @@ import torch
 import torch.nn.functional as F
 from torch.nn import Module, ModuleList
 
+from einops import rearrange
+
 from x_transformers import (
+    Encoder,
     RMSNorm,
-    Encoder
+    FeedForward
 )
 
 # helpers
@@ -18,13 +21,47 @@ def default(v, d):
 # class
 
 class SplineBasedTransformer(Module):
-    def __init__(self):
+    def __init__(
+        self,
+        dim,
+        enc_depth,
+        dec_depth = None,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.
+    ):
         super().__init__()
+        dec_depth = default(dec_depth, enc_depth)
+
+        self.encoder = Encoder(
+            dim = dim,
+            heads = heads,
+            depth = enc_depth,
+            attn_dim_head = dim_head,
+            attn_dropout = dropout,
+            ff_dropout = dropout
+        )
+
+        self.decoder = Encoder(
+            dim = dim,
+            heads = heads,
+            depth = dec_depth,
+            attn_dim_head = dim_head,
+            attn_dropout = dropout,
+            ff_dropout = dropout
+        )
 
     def forward(
         self,
         data,
         return_loss = False
     ):
-        recon = data.clone()
-        return data
+        encoded = self.encoder(data)
+
+        recon = self.decoder(encoded)
+
+        if not return_loss:
+            return recon
+
+        recon_loss = F.mse_loss(recon, data)
+        return recon_loss
